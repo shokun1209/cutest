a867ee220ecc92e1b120f36a653e69ad66f411f8
shokun1209 <shoya@satoushouyanoMacBook-Air.local>
2021-04-14 16:20:48 +0900

    photo_date カラム型修正

--- db/migrate/20210412125501_create_photos.rb
+++ db/migrate/20210412125501_create_photos.rb
@@ -3,6 +3,7 @@ class CreatePhotos < ActiveRecord::Migration[6.0]
     create_table :photos do |t|
       t.string :image
       t.date :photo_date
+      t.string :content_type
       t.references :uploader
       t.timestamps
     end
--- db/migrate/20210413071253_add_details_to_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddDetailsToPhotos < ActiveRecord::Migration[6.0]
-  def change
-    add_column :photos, :content_type, :string
-  end
-end
